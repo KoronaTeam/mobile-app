ce1766f052485a7019cf571cc328c3717f1e09d2
Radosław Kłos <admin@glxy.pl>
2020-04-24 20:29:23 +0200

    Vue property decorator

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,32 +1,31 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </Page>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!"/>
+    <GridLayout columns="*" rows="*">
+      <Label class="message" :text="$store.getters.getDupa" col="0" row="0"/>
+    </GridLayout>
+  </Page>
 </template>
 
 <script lang="ts">
-  export default {
-    data () {
-      return {
-        msg: 'Hello World!',
-      };
-    },
-  };
+  import { Vue, Component } from 'vue-property-decorator'
+
+  @Component
+  export default class Home extends Vue {
+
+  }
 </script>
 
-<style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+<style lang="scss" scoped>
+  ActionBar {
+    background-color: #53ba82;
+    color: #ffffff;
+  }
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+  .message {
+    vertical-align: center;
+    text-align: center;
+    font-size: 20px;
+    color: #333333;
+  }
 </style>
--- app/store.ts
+++ app/store.ts
@@ -4,7 +4,12 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    dupa: 'coś tam'
+  },
   mutations: {},
   actions: {},
+  getters: {
+
+  }
 });
