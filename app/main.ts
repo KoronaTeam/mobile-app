b8739cc63fdb6e6c4d11b11cc695a508cc82db60
Radosław Kłos <admin@glxy.pl>
2020-04-24 21:46:01 +0200

    Organizacja plików + devtoolsy

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,16 @@
 import VueDevtools from 'nativescript-vue-devtools';
 import Vue from 'nativescript-vue';
-import App from './components/App.vue';
-
-if (TNS_ENV !== 'production') {
-  Vue.use(VueDevtools);
-}
 import store from './store';
+import Home from './pages/Home.vue';
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
 
+if (TNS_ENV !== 'production') {
+  Vue.use(VueDevtools);
+}
+
 new Vue({
   store,
-  render: h => h('frame', [h(App)]),
+  render: h => h('frame', [h(Home)]),
 }).$start();
